71601c1d38350066b57a1a5a1565d19281e666d2
coleanderson98604 <shuffle2dabeat@gmail.com>
2018-04-25 10:36:19 -0500

    fixed missing room call

--- server.js
+++ server.js
@@ -109,6 +109,11 @@ var server = app.listen(8000, function(){
 var io = require('socket.io').listen(server);
 io.sockets.on('connect', function(socket) {
     console.log('new connection made.')
+
+    socket.on('roomCheck', function(){
+        socket.emit('rooms', io.sockets.adapter.rooms);
+    })
+
     socket.on('join', function(data){
         //joining, .join specifies a specific room for the user to join
         socket.join(data.room)
@@ -119,8 +124,6 @@ io.sockets.on('connect', function(socket) {
     });
 
     socket.on('leave', function(data){
-
-
         console.log(`${data.user} left the room: ${data.room}`)
         //broadcast to everyone except the person who is leaving, .to specifies which room to broadcast too
         socket.broadcast.to(data.room).emit('left room', {user: data.user, message:'has left this room.'});
